08f49577ac54709dae3a899111404ec0d9e2cbc7
AnahiVera <vera.anahi.93@gmail.com>
2025-03-03 18:58:23 -0300

    part 2.7

--- Part2/agendaTelefonica--/src/App.jsx
+++ Part2/agendaTelefonica--/src/App.jsx
@@ -4,17 +4,22 @@ import Name from './Name'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
+  ])
   const [newName, setNewName] = useState('')
-  
+
 
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
       name: newName
     }
-    setPersons(persons.concat(nameObject)) //concat crea una nueva copia de la matriz con el nuevo elemento agregado al final.
-    setNewName('')
+    //checks if the name already exists | Case sensitive
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(nameObject)) // concat creates a new copy of the array with the new element added at the end
+      setNewName('')
+    }
   }
 
   const handleNameChange = (event) => {
@@ -22,25 +27,27 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          name: 
-          <input 
-          value={newName} 
-          onChange={handleNameChange}/>
+          name:
+          <input
+            value={newName}
+            onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul> {persons.map(person => 
-        <Name key={person.name} name={person.name}/>)}
+      <ul> {persons.map(person =>
+        <Name key={person.name} name={person.name} />)}
       </ul>
-      
+
       ...
     </div>
   )
