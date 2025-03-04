528e36fed9eb3728e89ce59135c052ecc24af29c
AnahiVera <vera.anahi.93@gmail.com>
2025-03-04 17:50:44 -0300

    Part 2.11

--- Part2/agendaTelefonica--/src/App.jsx
+++ Part2/agendaTelefonica--/src/App.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Name from './Name'
-import Filter from './assets/Filter'
-import PersonForm from './assets/PersonForm'
+import Filter from './Filter'
+import PersonForm from './PersonForm'
+import axios from 'axios'
+
+
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'contacts')
 
 
   const addContact = (event) => {
